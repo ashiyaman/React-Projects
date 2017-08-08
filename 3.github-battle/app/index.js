4859123a924edd3a22d6a627d711f439d3afcc95
ashiyaman <ashiya1005@gmail.com>
2017-08-08 16:56:52 +1000

    Added filter

--- 3.github-battle/app/index.js
+++ 3.github-battle/app/index.js
@@ -1,40 +1,45 @@
-var React = require('react');
-// need react-dom coz we will be rendering the react component that we make to the DOM
-var ReactDOM = require('react-dom');
-// whatever css is in index.css will be applied to the componenet when everything bundles.
-// hence we need webpack
-require('./index.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
+class User extends React.Component {
+  render() {
+    var friends = this.props.list.filter(function(user) {
+      return user.friend === true
+    });
 
-// Properties associated with componenet
-// 1. state
-// 2. lifecycle event (eg. when should it be shown to the screen)
-// 3. UI( compulsory )
-
-class App extends React.Component {
-// how to tell the UI associated with it -> using render method
-// every component should have a render method
-// whatever the render returns is the UI of the component
-render() {
-
-  return (
+    var nonFriends = this.props.list.filter(function(user) {
+      return user.friend === false
+    });
     
-    // the div is not HTML(though it may look like), its JSX
-    // this isnt valid jS syntax
-    // hence we need babel which transpiled this code to like
-    // return React.createElement (
-    //  "div",
-    //  null,
-    //  "Hello world!"
-    // )
-    // the above is JS
-    // we can also write like this but its not easy to code like that
-    <div>
-      Hello World
-    </div>
-  )
-}
+    return (
+      <div>
+        <h1>Friends</h1>
+          <ul>
+            {friends.map(function(user) {
+              return <li>{user.name}</li>
+            })}
+          </ul>
+        <h1>Non-Friends</h1>
+          <ul>
+            {nonFriends.map(function(user) {
+              return <li>{user.name}</li>
+            })}
+          </ul>
+      </div>
+    )
+  }
 }
 
-// to use a component -> <APp />
-ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render(
+  <User list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]}
+  />,
+  document.getElementById('app')
+);
