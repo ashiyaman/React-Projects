7db97896f849e9a9a3f9be65338f1f20beaa2f69
Ashiya 1005 <ashiya1005@gmail.com>
2017-08-15 17:34:11 +1000

    Added for production

--- 3.github-battle/webpack.config.js
+++ 3.github-battle/webpack.config.js
@@ -1,7 +1,14 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+// we are directly requiring webpack coz it allows us tp do these 2 things
+// 1. need to add NODE_ENV variable to production
+//    ->let react know that we need to build our app in production module
+// 2. also add uglify/ minify all of our code
 
-module.exports = {
+// if we are building our application for production,
+// we want to add certain plugins to our config object
+config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -29,3 +36,24 @@ module.exports = {
      })
    ]
 };
+
+// we are setting NODE_ENV variable both in package.json(scripts->build) and here coz
+//    -> when we set NODE_ENV variable to production in package.JSON
+//    -> that is going to set the NODE_ENV variable in webpack.config file but
+//    -> not in compiled code that webpack is going to compile
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    // create new webpack plugin  and set process.env property to production
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        // the way we say react that we want to use the production version of react
+        //  -> setting the NODE_ENV variable of process.env object of react to production
+        //  -> react will then strip off any warnings or console.log and build specifically for production
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
+module.exports = config;
